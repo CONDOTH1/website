5dbe89d14cb8ee3a65149238c4e6effcbf0f0968
Thady Condon <thadycondon@hotmail.com>
2016-11-10 11:59:21 +0000

    organised the directory structure and started the sports section.

--- public/website_jquery.js
+++ public/website_jquery.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   var boxDivs = {home: $("#photoFrame"), about: $("#aboutTextBox"), contact: $('#contactTextBox'), sports: $('#triviaSport'), movies: $('#triviaMovies')};
 
-  var boxText = {home: "./lib/photo.erb", about: "./lib/about.erb", contact: "./lib/contact.erb", sports: "./lib/sports.erb", movies: "./lib/movies.erb"};
+  var boxText = {home: "./lib/erb/photo.erb", about: "./lib/erb/about.erb", contact: "./lib/erb/contact.erb", sports: "./lib/erb/sports.erb", movies: "./lib/erb/movies.erb"};
 
   var openBoxes = {home: true, about: false, contact: false, sports: false, movies: false};
 
@@ -15,27 +15,36 @@ $(document).ready(function(){
 
   websiteStartAnimation();
 
-
   $('#home').click(function(){
-    openBox("home", homePositions);
-    $("#quote").delay(3000).fadeIn("slow");
-});
+    if (animationNotRunning()) {
+      openBox("home", homePositions);
+      $("#quote").delay(3000).fadeIn("slow");
+    }
+  });
 
   $('#about').click(function(){
+    if (animationNotRunning()) {
       openBox("about", aboutPositions);
+    }
   });
 
   $('#contact').click(function(){
-    openBox("contact", contactPositions);
+    if (animationNotRunning()) {
+      openBox("contact", contactPositions);
+    }
   });
 
   $('#sports').click(function(){
-    openBox("sports", triviaPositions);
-    });
+    if (animationNotRunning()) {
+      openBox("sports", triviaPositions);
+    }
+  });
 
   $('#movies').click(function(){
-    openBox("movies", triviaPositions);
-    });
+    if (animationNotRunning()) {
+      openBox("movies", triviaPositions);
+    }
+  });
 
   function openBox(box, positions) {
     if (openBoxes[box] === false) {
@@ -43,7 +52,7 @@ $(document).ready(function(){
       $.when(element).then(function() {
         Box.animateForward(boxDivs[box], boxText[box], positions);
         openBoxes[box] = true;
-      })
+      });
     }
   }
 
@@ -62,16 +71,20 @@ $(document).ready(function(){
       .animate({right: "0%"}, 500)
       .animate({width: 220, height: 220}, 500)
       .animate({borderWidth: 5}, "slow");
-    $("#photoFrame").load("./lib/photo.erb");
+    $("#photoFrame").load("./lib/erb/photo.erb");
     loadQuote();
   }
 
   function loadQuote(){
-    $("#quote").load("./lib/quote.erb");
+    $("#quote").load("./lib/erb/quote.erb");
     for (var key in boxDivs) {
       if (boxDivs[key] !== 'home') {
         boxDivs[key].load(boxText[key]);
       }
-    };
+    }
+  }
+
+  function animationNotRunning(){
+    return $(':animated').length === 0;
   }
 });
